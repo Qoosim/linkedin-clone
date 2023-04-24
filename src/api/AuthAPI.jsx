44b394f1146185d52c151496e9755bf239dcb241
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-04-24 12:02:33 +0100

    Create register form to create users inside the firebase

--- src/api/AuthAPI.jsx
+++ src/api/AuthAPI.jsx
@@ -1,1 +1,20 @@
-export const LoginAPI = () => {}
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth} from '../firebaseConfig';
+
+export const LoginAPI = (email, password) => {
+  try {
+    let response = signInWithEmailAndPassword(auth, email, password);
+    return response;
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+export const RegisterAPI = (email, password) => {
+  try {
+    let response = createUserWithEmailAndPassword(auth, email, password);
+    return response;
+  } catch (error) {
+    return error;
+  }
+}
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { LoginAPI, RegisterAPI } from '../api/AuthAPI'
+import '../Sass/LoginComponent.scss';
 
 export const LoginComponent = () => {
+  const [credentials, setCredentials] = useState({});
+  const login = async () => {
+    try {
+      let res = await RegisterAPI(credentials.email, credentials.password);
+      console.log(res);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
-    <div>
-      <h2>LoginComponent</h2>
+    <div className='login-wrapper'>
+      <h1>LoginComponent</h1>
+      <div className='auth-inputs'>
+        <input 
+          onChange={(event) => setCredentials({ ...credentials, email: event.target.value })}
+          className='common-input'
+          placeholder="Enter your email here..."
+        />
+         <input 
+          onChange={(event) => setCredentials({ ...credentials, password: event.target.value })}
+          className='common-input'
+          placeholder="Enter your password here..."
+        />
+      </div>
+      <button onClick={login} className='login-btn'>Login to linkedin</button>
     </div>
   )
 }
