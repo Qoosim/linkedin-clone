5ad6a4267530159154c6ec59b6f63aa88880b94a
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-04-24 10:13:07 +0100

    Create react skeleton, login router

---
+++ src/components/LoginComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LoginComponent = () => {
+  return (
+    <div>
+      <h2>LoginComponent</h2>
+    </div>
+  )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBG4vLhSk1XzEygBNfbRSdB06l5_ims1AQ",
+  authDomain: "linkedin-clone-181bf.firebaseapp.com",
+  projectId: "linkedin-clone-181bf",
+  storageBucket: "linkedin-clone-181bf.appspot.com",
+  messagingSenderId: "922682235163",
+  appId: "1:922682235163:web:d0ba7dbd426968a22f1bbe",
+  measurementId: "G-JXGZNQ9DQ8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
